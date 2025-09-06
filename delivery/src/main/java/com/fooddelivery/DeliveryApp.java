ae8b813f431428feb15d3230b771215efa092fa4
sshehrozali <sshehroza13@gmail.com>
2025-09-06 20:05:20 +0500

    added

---
+++ delivery/src/main/java/com/fooddelivery/DeliveryApp.java
@@ -0,0 +1,13 @@
+package com.fooddelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryApp.class, args);
+    }
+
+}
---
+++ rider/src/main/java/com/fooddelivery/RiderApp.java
@@ -0,0 +1,13 @@
+package com.fooddelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiderApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiderApp.class, args);
+    }
+
+}
