c4ac8158c4a7023c820dc20b9376865ea90a9809
shehrozali <sshehroza13@gmail.com>
2025-09-08 17:11:49 +0500

    added comments

--- rider/src/main/java/com/fooddelivery/RiderService.java
+++
@@ -1,4 +0,0 @@
-package com.fooddelivery;
-
-public class RiderService {
-}
---
+++ rider/src/main/java/com/fooddelivery/RiderWebSocketService.java
@@ -0,0 +1,9 @@
+package com.fooddelivery;
+
+public class RiderWebSocketService {
+
+    // rider has subscribed to /search/orders topic
+    // riders gets an in-app notification for new order
+    // rider accepts
+    // dispatches notification to customer and rider
+}
